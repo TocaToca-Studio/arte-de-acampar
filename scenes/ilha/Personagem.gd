b107341a779df1080289cdbd36097ea9d3a14f8c
Eliezer Solinger <eliezersolinger@gmail.com>
2023-01-26 21:46:11 -0300

    adicionado arvore de teste

--- scenes/ilha/Personagem.gd
+++ scenes/ilha/Personagem.gd
@@ -15,7 +15,7 @@ export (float) var velocidade=3
 func pular():
 	pular=true
  
-
+var pulou_andando=false
 
 func _physics_process(delta): 
 	#gravity
@@ -27,13 +27,20 @@ func _physics_process(delta):
 	var no_chao=piso_detector.is_colliding()
 	#jump
 	if pular and no_chao:
+		pulou_andando=andando;
 		velocity.y = JUMP_HEIGHT
 		pular=false
 		
-		
-	player_speed=lerp(player_speed, velocidade if andando and no_chao else 0,delta*3)
 	
-	velocity+=-get_global_transform().basis.z.normalized()*player_speed
+	var mov=0
+	if no_chao:
+		if andando:mov=velocidade
+	else:
+		if pulou_andando: mov=velocidade
+		
+	player_speed=lerp(player_speed, mov,delta*3)
+	
+	velocity+=-(get_global_transform().basis.z.normalized())*player_speed
 		
 	gravity_speed = move_and_slide(velocity).y
 	
